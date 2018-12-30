42784bd91a933b2146124b089fc6879ebd566c5a
suremotoo <suremotoo@163.com>
2018-12-30 21:27:16 +0800

    [empty] initial commit

--- assets/normalize-shortcuts.js
+++
@@ -1,6 +0,0 @@
-let normalize = require('electron-shortcut-normalizer')
-let shortcuts = document.querySelectorAll('kbd.normalize-to-platform')
-
-Array.prototype.forEach.call(shortcuts, function (shortcut) {
-  shortcut.innerText = normalize(shortcut.innerText, process.platform)
-})
